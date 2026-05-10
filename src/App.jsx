dde09d9045f968e090b3ad4d5ee3fbda40515a4e
Nolido13 <odilonkiniffo@gmail.com>
2026-05-10 22:25:27 +0100

    commit du 10/05/26 à 22:25

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
         <WhatsAppButton />
         <CallButton />
         <TikTokButton />
-        <SwitchButton />
+        {/* <SwitchButton /> */}
       </div>
     </CityProvider>
   )
